2afc4c0faaf805dc1bc6d5535f773ea5b954651f
AleksandrLaktionov <72191399+AleksandrLaktionov@users.noreply.github.com>
2022-01-26 12:09:36 +0300

    styling button

--- src/components/Todos/ListScrolling/ListScrolling.jsx
+++ src/components/Todos/ListScrolling/ListScrolling.jsx
@@ -1,10 +1,12 @@
+import './ListScrolling.scss'
+
 const ListScrolling = ({ getNextList, getPreviousList }) => {
   return (
     <div className="list-scrolling">
       <button onClick={e => {
         getPreviousList(5)
       }}>{'<<<'}</button>
-      ...
+      {[...'...'].map(i=><span></span>)}
       <button onClick={e => {
         getNextList(5)
       }}>{'>>>'}</button>
