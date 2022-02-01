10d550d11287b799f17ea3ee95eec7b36eb1fc19
AleksandrLaktionov <72191399+AleksandrLaktionov@users.noreply.github.com>
2022-02-01 14:00:23 +0300

    styling a clock

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -5,20 +5,33 @@ const Clock = () => {
 
   const [state, setStat] = useState(new Date())
 
-  useEffect(() => setInterval(() => setStat(new Date()), 1000))
+  useEffect(() => setTimeout(() => setStat(new Date()), 1000))
 
   return (
     <div className='clock'>
+      <span id='center'></span>
       <span id='hours' style={{
-        transform: `rotate(${state.getHours() * 30}deg)`
+        transform: `rotate(${state.getHours() * 30 - 180}deg)`
       }}></span>
       <span id='minutes' style={{
-        transform: `rotate(${state.getMinutes() * 6}deg)`
+        transform: `rotate(${state.getMinutes() * 6 - 180}deg)`
       }}></span>
       <span id='seconds' style={{
         background: 'red',
-        transform: `rotate(${state.getSeconds() * 6}deg)`
+        transform: `rotate(${state.getSeconds() * 6 - 180}deg)`
       }}></span>
+      <span className='time__breakfast'>9</span>
+      <span className='time__lunch'>12</span>
+      <span className='time__afternoon-tea'>3</span>
+      <span className='time__dinner'>6</span>
+      <span className='time__1'></span>
+      <span className='time__2'></span>
+      <span className='time__4'></span>
+      <span className='time__5'></span>
+      <span className='time__7'></span>
+      <span className='time__8'></span>
+      <span className='time__10'></span>
+      <span className='time__11'></span>
     </div>);
 };
 
